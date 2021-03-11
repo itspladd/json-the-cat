9d1f6740d5da150251e342f6ca377181201271b9
Paul Ladd <vlendle@gmail.com>
2021-03-11 20:02:28 +0000

    Added basic search functionality from command line

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
+const breedSearchTerm = process.argv[2];
 
-request.get("https://api.thecatapi.com/v1/breeds/search?q=sib", (err, response, body) => {
+request.get("https://api.thecatapi.com/v1/breeds/search?q=" + breedSearchTerm, (err, response, body) => {
   console.log(err);
   console.log(response);
   console.log(JSON.parse(body)[0].description);
