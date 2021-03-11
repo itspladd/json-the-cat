d039a1481b20d32c884afaa847ffb935a9f8116c
Paul Ladd <vlendle@gmail.com>
2021-03-11 20:22:42 +0000

    Adding index.js, checked for import/export working

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,24 +1,29 @@
 const request = require('request');
 const breedSearchTerm = process.argv[2];
 
-if (!breedSearchTerm) {
-  console.log("No search term given. Try again!");
-  return;
-}
-
-request.get("https://api.thecatapi.com/v1/breeds/search?q=" + breedSearchTerm, (err, response, body) => {
-  if (err) {
-    console.log(err);
+const fetchBreedDescription = (searchTerm, callback) => {
+  if (!searchTerm) {
+    console.log("No search term given. Try again!");
     return;
   }
-  const results = JSON.parse(body);
-  if (!results.length) {
-    console.log("Error: Breed not found.");
-  } else {
-    console.log("Breed search results:");
-    results.forEach(element => {
-      console.log(`${element.name}: ${element.description}`);
-      console.log(`******************************`);
-    });
-  }
-});
+  
+  request.get("https://api.thecatapi.com/v1/breeds/search?q=" + searchTerm, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const results = JSON.parse(body);
+    if (!results.length) {
+      console.log("Error: Breed not found.");
+    } else {
+      console.log("Breed search results:");
+      results.forEach(element => {
+        console.log(`${element.name}: ${element.description}`);
+        console.log(`******************************`);
+      });
+    }
+  });
+}
+console.log("import/export worked")
+
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,1 @@
+const fetchBreedDescription = require('./breedfetcher');
