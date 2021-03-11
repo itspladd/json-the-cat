a2c84ea488aa3e29f1bb0b9bb51f42593a87c319
Paul Ladd <vlendle@gmail.com>
2021-03-11 20:08:08 +0000

    Added handling for no input arguments

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,6 +1,11 @@
 const request = require('request');
 const breedSearchTerm = process.argv[2];
 
+if (!breedSearchTerm) {
+  console.log("No search term given. Try again!");
+  return;
+}
+
 request.get("https://api.thecatapi.com/v1/breeds/search?q=" + breedSearchTerm, (err, response, body) => {
   console.log(err);
   //console.log(response);
