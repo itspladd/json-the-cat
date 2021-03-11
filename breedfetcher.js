02931e351965c3b5b24f6ad16aac219adb445e6d
Paul Ladd <vlendle@gmail.com>
2021-03-11 20:00:48 +0000

    Printing out just the description

--- breedfetcher.js
+++ breedfetcher.js
@@ -3,5 +3,5 @@ const request = require('request');
 request.get("https://api.thecatapi.com/v1/breeds/search?q=sib", (err, response, body) => {
   console.log(err);
   console.log(response);
-  console.log(JSON.parse(body));
+  console.log(JSON.parse(body)[0].description);
 })
