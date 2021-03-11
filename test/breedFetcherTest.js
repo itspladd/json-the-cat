050a357a2579fdc4f4282280dc72bedfbb3f65c0
Paul Ladd <vlendle@gmail.com>
2021-03-11 21:00:39 +0000

    added a test for no results and fixed a test bug

--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -5,16 +5,25 @@ const fetchBreedDescription = require('../breedfetcher');
 describe('#fetchBreedDescription', () => {
   it(`shoud return a single-item array with breed results for a valid breed, via a callback`, (done) => {
     fetchBreedDescription('Siberian', (err, results) => {
-      assert.equal(err, null);
+      assert.isNull(err);
 
       const expected = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ";
-      assert.equal(desc.length, 1);
-      assert.equal(desc[0].description, expected);
+      assert.equal(results.length, 1);
+      assert.equal(results[0].description, expected);
 
       done();
     });
   });
-  it(`should...`);
+  it(`shoud return an error and null results for no search results`, (done) => {
+    fetchBreedDescription('Shih Tzu', (err, results) => {
+      assert.isNull(results);
+      
+      const expected = "Error: No results for that search term.";
+      assert.equal(err, expected);
+
+      done();
+    });
+  });
   it(`should...`);
   it(`should...`);
 });
