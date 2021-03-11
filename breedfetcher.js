65092b1bcdafa34fcc6bd654c4307a77439db5e6
Paul Ladd <vlendle@gmail.com>
2021-03-11 20:06:59 +0000

    Added basic error handling for no search results found

--- breedfetcher.js
+++ breedfetcher.js
@@ -3,6 +3,10 @@ const breedSearchTerm = process.argv[2];
 
 request.get("https://api.thecatapi.com/v1/breeds/search?q=" + breedSearchTerm, (err, response, body) => {
   console.log(err);
-  console.log(response);
-  console.log(JSON.parse(body)[0].description);
+  //console.log(response);
+  if (body = "[]") {
+    console.log("Error: Breed not found.");
+  } else {
+    const results = JSON.parse(body);
+  }    
 })
